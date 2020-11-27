59b933be6fb0abdac2fdd02a8bd1341bd72a96dd
jramos <juanramosgarcia@protonmail.com>
2020-11-27 20:29:23 +0100

    docker-all no longer create daemon created onlyRecorder docker-compose profile include mempoolRecorder's Volumes into rmVolumes.sh

--- docker-all.sh
+++ docker-all.sh
@@ -7,43 +7,43 @@ cd "$(dirname "$0")"
 cd ../configurationServer
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../serviceDiscovery
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../gatewayService
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../txMemPool
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../bitcoindAdapter
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../mempoolRecorder
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 cd ../mempoolServer
 echo 
 echo "Creating project image" $(pwd) "....."
-./gradlew bootJar docker
+./gradlew bootJar docker --no-daemon
 
 cd "$(dirname "$0")"
 echo 
