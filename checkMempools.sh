874a04603faee0143452f6bcbcaa0780ec2bf1be
jramos <juanramosgarcia@protonmail.com>
2021-03-26 08:51:03 +0100

    various changes in infraestructure

--- checkMempools.sh
+++ checkMempools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "bitcoind:" && \
-~/PROGRAMS/bitcoin-master/src/bitcoin-cli --rpcconnect=pc getmempoolinfo && \ 
+~/programas/bitcoin-0.21.0/bin/bitcoin-cli --rpcconnect=pc getmempoolinfo && \
 echo "" && echo "bitcoindAdapter1:" && curl http://bitcoindadapter1:8080/memPool/size && \
 #echo "" && echo "bitcoindAdapter2:" && curl http://bitcoindadapter2:8080/memPool/size && \
-echo "" && echo "txmempool1:" && curl http://txmempool1:8080/memPool/size && \
+#echo "" && echo "txmempool1:" && curl http://txmempool1:8080/memPool/size && \
 #echo "" && echo "txMempool2:" && curl http://txmempool2:8080/memPool/size && \
 #echo "" && echo "txMempool3:" && curl http://txmempool3:8080/memPool/size && \
 #echo "" && echo "mempoolRecorder1:" && curl http://mempoolRecorder1:8080/mempool/size && \
